f6dcfb673db3b4d8de8ed25d121216335e49e6a1
David Fyfe <david@davidfyfe.net>
2023-03-18 12:27:31 +1100

    All tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,35 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        
-        unimplemented!("Revive this player")
+        if self.health == 0 {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        } else {
+            None
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {mana_cost}")
+        match self.mana {
+            Some(mana) => {
+                if mana >= mana_cost {
+                    self.mana = Some(mana - mana_cost);
+                    mana_cost * 2
+                } else {
+                    0
+                }
+            }
+            None => {
+                if self.health > mana_cost {
+                    self.health -= mana_cost;
+                } else {
+                    self.health = 0;
+                }
+                0
+            }
+        }
     }
 }
